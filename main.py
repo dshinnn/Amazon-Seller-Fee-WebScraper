38d49f9c82553b0012ac36adb7a55fc7dbdc6aa3
David Shin <shin.david93@gmail.com>
2023-04-27 15:40:36 -0700

    Fix last prices and oversize prices not showing up

--- main.py
+++ main.py
@@ -33,26 +33,40 @@ def Referral_Fees():
 
     return data
 
-def FBA_Fulfillment_Fees(type=3):
+def FBA_Fulfillment_Fees(type=1):
     data = []
     size_and_dimension = []
     shipping_weight = []
     non_apparel_fees = []
     apparel_fees = []
     small_and_light_fees = []
+    oversize_fees = []
     is_first_set = True
 
-    for index in range(len(table)):
-        INDEX_TEXT = table[index].get_text()
-        # Gets fulfillments fees for apparel items 
-        if index in range(195, 199) or index in range(210, 218) and type == 1:
-            apparel_fees.append(INDEX_TEXT)
+    # Creates prices for FBA categories if requested
+    for INDEX in range(len(table)):
+        INDEX_TEXT = table[INDEX].get_text()
         # Gets fulfillments fees for non-apparel items 
-        if index >= 149 and index <= 152 or index >= 164 and index <= 172 and type == 2:
+        if INDEX >= 149 and INDEX <= 152 or INDEX >= 164 and INDEX < 172 and type == 1:
             non_apparel_fees.append(INDEX_TEXT)
+        # Gets fulfillments fees for apparel items 
+        if INDEX in range(195, 199) or INDEX in range(210, 218) and type == 2:
+            apparel_fees.append(INDEX_TEXT)
         # Gets fulfillments fees for Small and Light items
-        if index in range(241, 245) or index in range(255, 262) or index in [152, 168] and type == 3:
+        if INDEX in range(241, 245) or INDEX in range(255, 262) or INDEX == 152 and type == 3:
             small_and_light_fees.append(INDEX_TEXT)
+        # Gets oversize fees for non-apparel and apparel items
+        if INDEX in [176, 180, 184, 188] and INDEX_TEXT not in oversize_fees:
+            oversize_fees.append(INDEX_TEXT)
+
+    # Manually appends last fee to lists below (indices being reused mixes the order from the scrape)
+    # Note: In this case, order is important due to the zip function for the final print out
+    if type == 1:
+        non_apparel_fees.append(table[172].get_text())
+    if type == 2:
+        apparel_fees.append(table[172].get_text())
+    if type == 3:
+        small_and_light_fees.append(table[168].get_text())
 
     # Gets common data for the fulfillment fees
     for row in table:
@@ -73,36 +87,36 @@ def FBA_Fulfillment_Fees(type=3):
             
             # Gets Shipping Weight column values
             if ROW_INDEX >= 145 and ROW_INDEX <= 148 or ROW_INDEX > 154 and ROW_INDEX <= 163:
-                shipping_weight.append(ROW_TEXT)
-            
+                if ROW_TEXT not in shipping_weight:
+                    shipping_weight.append(ROW_TEXT)
+
     # Removes duplicates in the shipping_weight list
-    del shipping_weight[0:4]
-    del small_and_light_fees[0:2]
+    del small_and_light_fees[0]
 
     # Gets fees for non-apparel items
-    if type == 1:
+    if type == 1 or type == 4:
         # Manually formats rows for print
         data.append([size_and_dimension[0], size_and_dimension[1], list(zip(shipping_weight[0:4], non_apparel_fees[0:4]))])     # Small Standard
         data.append([size_and_dimension[2], size_and_dimension[3], list(zip(shipping_weight[0:9], non_apparel_fees[4:13]))])    # Large Standard
     
     # Gets fees for apparel items
-    if type == 2:
+    if type == 2 or type == 4:
         data.append([size_and_dimension[0], size_and_dimension[1], list(zip(shipping_weight[0:4], apparel_fees[0:4]))])     # Small Standard
         data.append([size_and_dimension[2], size_and_dimension[3], list(zip(shipping_weight[0:9], apparel_fees[4:13]))])    # Large Standard
 
+    # Appends to data only if apparel or non-apparel fees are requested
+    if type == 1 or type == 2 or type == 4:    
+        # Identical for both apparel and non-apparel fees
+        data.append([size_and_dimension[4:7], oversize_fees[0]])      # Small Oversize
+        data.append([size_and_dimension[8:11], oversize_fees[1]])     # Medium Oversize
+        data.append([size_and_dimension[12:15], oversize_fees[2]])    # Large Oversize
+        data.append([size_and_dimension[16:19], oversize_fees[3]])    # Special Oversize
+        
     # Gets fees for small and light items
-    if type == 3:
+    if type == 3 or type == 4:
         data.append([table[235].get_text(), size_and_dimension[1], list(zip(shipping_weight[0:4], small_and_light_fees[0:4]))])     # Small Standard
         data.append([table[245].get_text(), size_and_dimension[3], list(zip(shipping_weight[0:8], small_and_light_fees[4:12]))])    # Large Standard        
     
-    # Appends to data only if apparel or non-apparel fees are requested
-    if type == 1 or type == 2:    
-        # Identical for both apparel and non-apparel fees
-        data.append(size_and_dimension[4:7])      # Small Oversize
-        data.append(size_and_dimension[8:11])     # Medium Oversize
-        data.append(size_and_dimension[12:15])    # Large Oversize
-        data.append(size_and_dimension[16:19])    # Special Oversize
-
     return data
 
 def Storage_Fees():
@@ -112,6 +126,6 @@ def Storage_Fees():
     return data
 
 if __name__ == '__main__':
-    # FBA_Fulfillment_Fees()
+    print(FBA_Fulfillment_Fees(2))
     # Referral_Fees()
-    Storage_Fees()
+    # Storage_Fees()
